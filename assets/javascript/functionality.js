8a7e29bdcf1e2a46ba3c4872d8bdcf0a9db1e05d
Ethan Hart <ethanhart35@gmail.com>
2021-03-01 19:31:22 -0700

    added nav links to about me page and added effect for nav bar

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -1,36 +1,43 @@
-
-$(document).ready(function(){
-    $(".p").hide()
+$(document).ready(function () {
 
 
 
-    $(".logo").click(function(){
-            if(this.innerHTML=="Ethan Hart"){
-                $(".logo").fadeOut("slow", function(){
-                this.innerHTML="Hello There"
-              })
-                $(".logo").fadeIn("slow", function(){
-                    return
-                })
-            }
-            if(this.innerHTML=="Hello There"){
-                $(".logo").fadeOut("slow", function(){
-                    this.innerHTML="Ethan Hart"
-                })
-                $(".logo").fadeIn("slow", function(){
-                    return
-                })
-              }
+
+    $(".logo").click(function () {
+        if (this.innerHTML == "Ethan Hart") {
+            $(".logo").fadeOut("slow", function () {
+                this.innerHTML = "Hello There"
+            })
+            $(".logo").fadeIn("slow", function () {
+                return
+            })
+        }
+        if (this.innerHTML == "Hello There") {
+            $(".logo").fadeOut("slow", function () {
+                this.innerHTML = "Ethan Hart"
+            })
+            $(".logo").fadeIn("slow", function () {
+                return
+            })
+        }
     });
 
-    function fade(){
-        $(".p").fadeIn("slow",function(){
-            return
+    $(".p").hide();
+    $(".about").hide();
+
+    function fade() {
+
+        $(".about").fadeIn("slow", function(){
+            $(".p").fadeIn("slow", function (){
+            
+            })
         })
+
+        
     }
 
 
-    setTimeout(fade, 1000)
+    setTimeout(fade, 500)
 
 
 
@@ -38,4 +45,4 @@ $(document).ready(function(){
 
 
 
-  });
+});
