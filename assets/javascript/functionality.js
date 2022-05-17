034f7d094d4f7060f1bbce730a71331d56eced35
Ethan Hart <ethanhart35@gmail.com>
2022-05-17 12:46:00 -0700

    Updating look
    Removed now unused code and starting new with fullpage.js in mind.

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -1,46 +1,30 @@
-
-
 $(document).ready(function () {
 
-
-
-
-    $(".logo").click(function () {
-        if (this.innerHTML == "Ethan Hart") {
-            $(".logo").fadeOut("slow", function () {
-                this.innerHTML = "What are you doing in my swamp?"
-            })
-            $(".logo").fadeIn("slow", function () {
-                return
-            })
-        }
-        if (this.innerHTML == "Hello!") {
-            $(".logo").fadeOut("slow", function () {
-                this.innerHTML = "Ethan Hart"
-            })
-            $(".logo").fadeIn("slow", function () {
-                return
-            })
-        }
-    });
 
     $(".p").hide();
     $(".about").hide();
 
     function fade() {
-        $(".about").fadeIn("slow", function () {
+        $(".about").fadeIn("slow", function () {})
+    }
+
+    function aboutMe() {
+        $(".aboutUnderline").css({
+            "margin-left": "33%",
+            "margin-right": "33%",
+            "width": "33%",
+            "opacity": "50%",
+            "transition": ".6s ease-in-out"
         })
     }
-    function aboutMe(){
-        $(".aboutUnderline").css({"margin-left":"33%","margin-right":"33%","width":"33%","opacity":"50%","transition":".6s ease-in-out"})
-    }
-    function paragraph(){
-        $(".p").fadeIn("slow", function(){
+
+    function paragraph() {
+        $(".p").fadeIn("slow", function () {
 
         })
     }
 
     setTimeout(fade, 500)
-    setTimeout(aboutMe,1000)
-    setTimeout(paragraph,1700)
+    setTimeout(aboutMe, 1000)
+    setTimeout(paragraph, 1700)
 });
