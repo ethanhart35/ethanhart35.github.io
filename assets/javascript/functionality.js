657e4141a61a99366f293821c3ecf83abd393f35
Ethan Hart <ethanhart35@gmail.com>
2022-05-21 23:34:30 -0700

    Removed unused Javascript

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -1,30 +1,3 @@
 $(document).ready(function () {
 
-
-    $(".p").hide();
-    $(".about").hide();
-
-    function fade() {
-        $(".about").fadeIn("slow", function () {})
-    }
-
-    function aboutMe() {
-        $(".aboutUnderline").css({
-            "margin-left": "33%",
-            "margin-right": "33%",
-            "width": "33%",
-            "opacity": "50%",
-            "transition": ".6s ease-in-out"
-        })
-    }
-
-    function paragraph() {
-        $(".p").fadeIn("slow", function () {
-
-        })
-    }
-
-    setTimeout(fade, 500)
-    setTimeout(aboutMe, 1000)
-    setTimeout(paragraph, 1700)
 });
