b9ac7ed748e31053903d94bfc6e4b4133d2141bf
Ethan Hart <ethanhart35@gmail.com>
2022-08-11 16:18:03 -0700

    Added animation for my name

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -1,3 +1,7 @@
-$(document).ready(function () {
 
-});
+
+const logo = document.querySelectorAll('#logoname path');
+
+for (let i = 0; i<logo.length; i++) {
+    console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
+}
