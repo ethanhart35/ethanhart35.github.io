b6d22953042c3b2e0ff3c3ca7e2a8864df967262
Ethan Hart <ethanhart35@gmail.com>
2022-08-11 22:48:02 -0700

    Added load on scroll effect

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -5,3 +5,19 @@ const logo = document.querySelectorAll('#logoname path');
 for (let i = 0; i<logo.length; i++) {
     console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
 }
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
